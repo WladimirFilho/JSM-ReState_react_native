d59c9e0491ce1d309dbde8f930567450524cd9d0
wladimirfilho <wladimir.wf@gmail.com>
2024-12-22 16:46:35 +1100

    first commit

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native'
+import React from 'react'
+
+const Explore = () => {
+    return (
+        <View>
+            <Text>Explore</Text>
+        </View>
+    )
+}
+export default Explore
