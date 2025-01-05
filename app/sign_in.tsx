45b2c42262154ba204416cd6fe25c01c00a7734d
wladimirfilho <wladimir.wf@gmail.com>
2025-01-05 23:19:38 +1100

    new changes

--- app/sign_in.tsx
+++ app/sign_in.tsx
@@ -1,41 +1,73 @@
-import {Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View} from 'react-native'
-import React from 'react'
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 
 const SingIn = () => {
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
+  if (!loading && isLoggedIn) return <Redirect href="/" />;
 
-    const handleLogin = () => {
-        // Simulate login with Google
-        console.log("Login with Google")
+  const handleLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
     }
-    return (
-        <SafeAreaView className="bg-white h-full">
-            <ScrollView contentContainerClassName="h-full">
-                <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"/>
+  };
 
-                <View className="px-10">
-                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to
-                        ReState</Text>
-                    <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">Let's Get You Closer
-                        to {'\n'}
-                        <Text className="text-primary-300">Your Ideal Home</Text>
-                    </Text>
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
 
-                    <Text className="text-lg font-rubik text-center mt-12 text-black-200">Login to ReState with
-                        Google</Text>
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
 
-                    <TouchableOpacity onPress={handleLogin}
-                                      className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
-                        <View className="flex flex-row items-center justify-center">
-                            <Image source={icons.google} className="w-5 h-5" resizeMode="contain"/>
-                            <Text className="text-lg font-rubik-medium text-black-300 ml-2">Continue with google</Text>
-                        </View>
+          <Text className="text-lg font-rubik text-center mt-12 text-black-200">
+            Login to ReState with Google
+          </Text>
 
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
-export default SingIn
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default SingIn;
